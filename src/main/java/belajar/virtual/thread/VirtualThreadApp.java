24944bf9c747a651851088ee871fb9e80e870459
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-10-14 17:13:12 +0700

    feat: add example virtual thread app

---
+++ src/main/java/belajar/virtual/thread/VirtualThreadApp.java
@@ -0,0 +1,24 @@
+package belajar.virtual.thread;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class VirtualThreadApp {
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 10_000; i++) {
+            Thread thread = Thread.ofVirtual().name("eko" + i).unstarted(() -> {
+                try {
+                    Thread.sleep(Duration.ofSeconds(2));
+                    System.out.println("Hello " + Thread.currentThread() + "!");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            thread.start();
+        }
+
+        System.in.read();
+    }
+}
